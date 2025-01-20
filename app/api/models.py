92da0f559413712cb891ec6df2f523d2a4c0842e
Douwe De Bock <douwedebock@gmail.com>
2025-01-20 21:13:47 +0100

    Add celery for background export generation

--- app/api/models.py
+++ app/api/models.py
@@ -9,10 +9,19 @@ class Record(models.Model):
     def __str__(self):
         return f'mapping of {self.persistent_url} to {self.resource_url}'
 
+    class Meta:
+        indexes = [
+            models.Index(name="not-enabled", fields=['enabled'], condition=models.Q(enabled=False)),
+        ]
+
 class RequestLog(models.Model):
     datetime = models.DateTimeField(auto_now=True)
     record = models.ForeignKey(Record, on_delete=models.CASCADE)
     referer = models.CharField(max_length=100, null=True)
 
     
+class Export(models.Model):
+    export_type = models.CharField(max_length=1) # 'R' for record, 'L' for log
+    datetime = models.DateTimeField(auto_now=True)
+    filename = models.CharField(max_length=100)
 
