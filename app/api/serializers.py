ad74a65b0620d3af849ade2c658df3c661cd221f
Douwe De Bock <douwedebock@gmail.com>
2026-08-07 20:12:59 +0200

    Force a slash in PURI check

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -29,9 +29,9 @@ class RecordSerializer(serializers.Serializer):
         if not settings.PURI_CHECK:
             return value
 
-        if value.startswith(settings.ALLOWED_HOSTS[0]):
+        if value.startswith(f"{settings.ALLOWED_HOSTS[0]}/"):
             return value
-        raise serializers.ValidationError("persistent_url should start with the domain name")
+        raise serializers.ValidationError("persistent_url should start with the domain name followed by a /")
 
 
 
